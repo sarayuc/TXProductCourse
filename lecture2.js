f5b4fd4a48b636a45082fb4368518fca18a2ea7e
sarayuc <98441112+sarayuc@users.noreply.github.com>
2022-03-03 19:05:51 -0600

    Create lecture2.js

---
+++ lecture2.js
@@ -0,0 +1,19 @@
+function triangleArea(base, height) {
+    return base * height * 0.5
+}
+
+var oneArea = triangleArea(5, 10);
+
+var twoArea = triangleArea(2, 4);
+
+var threeArea = triangleArea(4, 20);
+
+console.log(oneArea);
+console.log(twoArea);
+console.log(threeArea);
+
+function convertFeetToInches(length) {
+    return length * 12
+}
+
+
